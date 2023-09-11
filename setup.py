15797692aa4b3437d5f5d81b4c2c54f6e87110a5
sasha bouloudnine <sasha.bouloudnine@lobstr.io>
2023-09-11 22:05:14 +0200

    update setup.py add cli boi

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     description='scrape housing listings on bien\'ici from any bien\'ici search url 💛',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    url='https://github.com/lobstrio/bieniciscraper',  # Add your package's URL here
     author='sasha bouloudnine',
     author_email='sasha.bouloudnine@lobstr.io',
     packages=find_packages(),
@@ -21,4 +22,9 @@ setup(
             'bieniciscraper = bieniciscraper.main:main',
         ],
     },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
